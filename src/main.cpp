ff19890204cd7bb6a6d598e3ff3a02d0ef319289
JamesBremner <james@ravenspoint.com>
2024-12-07 09:51:06 -0500

    Some code

--- src/main.cpp
+++ src/main.cpp
@@ -4,31 +4,111 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <wex.h>
-#include "cStarterGUI.h"
 
-class cGUI : public cStarterGUI
+typedef std::string element_t;
+
+typedef std::vector<element_t> group_t;
+
+typedef std::vector<group_t> version_t;
+
+version_t v1, v2;
+
+struct sEditDist
 {
-public:
-    cGUI()
-        : cStarterGUI(
-              "Starter",
-              {50, 50, 1000, 500}),
-          lb(wex::maker::make < wex::label >(fm))
+    int iv1;
+    int iv2;
+    int dist;
+
+    void display()
     {
-        lb.move(50, 50, 100, 30);
-        lb.text("Hello World");
-
-        show();
-        run();
+        std::cout << iv1 << " " << iv2 << " " << dist << "\n";
     }
-
-private:
-    wex::label &lb;
 };
+
+typedef std::vector<sEditDist> vEdits_t;
+
+// optimal connections
+vEdits_t vConnections;
+
+/// @brief generate example from stackoverflow question
+/// @return
+/// https://stackoverflow.com/q/79258933/16582
+void gen1()
+{
+    // g0_0={a,b,c}, g0_1={d,e,f,g,h}, g0_2={i}, and g0_3={j}.
+    v1 = version_t{
+        {"a", "b", "c"},
+        {"d", "e", "f", "g", "h"},
+        {"i"},
+        {"j"}};
+
+    // g1_0={a,b,c,d}, g1_1={e,f,g,h,i,k}, g1_2={j,l}
+    v2 = version_t{
+        {"a", "b", "c", "d"},
+        {"e", "f", "g", "h", "i", "k"},
+        {"j", "l"}};
+}
+/// @brief edit distance between two groups
+/// @param g1
+/// @param g2
+/// @return
+int editDistance(
+    const group_t &g1,
+    const group_t &g2)
+{
+    int dist = 0;
+    for (auto &e : g1)
+    {
+        auto it = std::find(g2.begin(), g2.end(), e);
+        if (it == g2.end())
+            dist++;
+    }
+    for (auto &e : g2)
+    {
+        auto it = std::find(g1.begin(), g1.end(), e);
+        if (it == g1.end())
+            dist++;
+    }
+    return dist;
+}
+/// @brief calculate edit distances between every pair of groups in the versions
+void editDistance()
+{
+    sEditDist ed;
+    for (ed.iv1 = 0; ed.iv1 < v1.size(); ed.iv1++)
+    {
+        int bestv2 = 0;
+        int bestDist = INT_MAX;
+        for (ed.iv2 = 0; ed.iv2 < v2.size(); ed.iv2++)
+        {
+            // calculate edit distance
+            ed.dist = editDistance(
+                v1[ed.iv1],
+                v2[ed.iv2]);
+            ed.display();
+
+            // keep the smallest edit distance
+            if( ed.dist < bestDist) {
+                bestDist = ed.dist;
+                bestv2 = ed.iv2;
+            }
+        }
+        // add connection with smallest edit distance from g1
+        ed.iv2 = bestv2;
+        ed.dist = bestDist;
+        vConnections.push_back(ed);
+    }
+}
+void editDistanceDisplay()
+{
+    for (auto &d : vConnections)
+        d.display();
+}
 
 main()
 {
-    cGUI theGUI;
+    gen1();
+    editDistance();
+    editDistanceDisplay();
     return 0;
 }
